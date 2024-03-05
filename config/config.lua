3e529adab9e8985ed7f6cc0c2094fffbc15b5f29
drk playz <93538336+drkkkkkk@users.noreply.github.com>
2024-03-05 20:45:08 +0000

    Update config.lua
    Added the configuration for the Green Text at the top of the screen that appears upon running the /startafk command or your AFK Command!

--- config/config.lua
+++ config/config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.DiscordWebHook = "https://discord.com/api/webhooks/1214342218563985448/zh1U8ufgVZv7VvhYj1Epk-_hYE1XH06Ru0m8bTXs32r58gqL9lTA5M9rHVd3XivVDyYh"
+Config.DiscordWebHook = "DiscordWebHookHere"
 
 Config.AFKArea = vector3(749.6129, 1282.0568, 360.2964) -- Area player gets teleported to upon doing /afkstart or whatever you command is
 Config.AFKBack = vector3(151.3788, -1036.1140, 29.3393) -- Area player gets teleported to upon doing /afkstop or whatever you command is
@@ -10,3 +10,5 @@ Config.AFKStopCmd = "afkstop"
 
 Config.AFKStartColor = 65280 -- (This is Green, It's decimal.)
 Config.AFKStopColor = 16744576 -- (This is Red, It's a decimal.)
+
+Config.AFKTextMessage = "You're Now AFK!"
